4b220ab043695b858d9d7e1ff4fb0232d881e7d2
Equipe TDD <aula@tdd.local>
2026-09-14 12:28:51 -0300

    ciclo 2: triangulacao derruba o return false

--- src/main/java/br/edu/tdd/exemplo/ValidadorDeSenha.java
+++ src/main/java/br/edu/tdd/exemplo/ValidadorDeSenha.java
@@ -3,6 +3,6 @@ package br.edu.tdd.exemplo;
 public class ValidadorDeSenha {
 
     public boolean ehValida(String senha) {
-        return false;
+        return senha.length() >= 8;
     }
 }
--- src/test/java/br/edu/tdd/exemplo/ValidadorDeSenhaTest.java
+++ src/test/java/br/edu/tdd/exemplo/ValidadorDeSenhaTest.java
@@ -2,6 +2,7 @@ package br.edu.tdd.exemplo;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidadorDeSenhaTest {
 
@@ -10,5 +11,12 @@ class ValidadorDeSenhaTest {
         ValidadorDeSenha validador = new ValidadorDeSenha();
 
         assertFalse(validador.ehValida("Abc123"));
+    }
+
+    @Test
+    void senhaComOitoOuMaisCaracteresEhValida() {
+        ValidadorDeSenha validador = new ValidadorDeSenha();
+
+        assertTrue(validador.ehValida("Abcd1234"));
     }
 }
